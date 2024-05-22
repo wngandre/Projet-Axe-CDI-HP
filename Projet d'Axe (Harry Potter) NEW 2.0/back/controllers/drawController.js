984b9e29a06014d15eb1a529be963eb6c456984d
wngandre <148988341+wngandre@users.noreply.github.com>
2024-05-22 17:21:21 +0200

    drawController.js

--- Projet d'Axe (Harry Potter) NEW 2.0/back/controllers/drawController.js
+++ Projet d'Axe (Harry Potter) NEW 2.0/back/controllers/drawController.js
@@ -25,13 +25,13 @@ async function drawCards(req, res) {
     const userId = parseInt(req.params.userId);
 
     try {
-        const cards = await prisma.card.findMany();
-        const draw = [];
+        const cards = await prisma.card.findMany(); // On récupère tout les cartes dans l'API Lainocs
+        const draw = []; // SA TIRE 5 CARTE ALEATOIRE
         for (let i = 0; i < 5; i++) {
             draw.push(Math.floor(Math.random() * cards.length));
         }
 
-        for (const cardIndex of draw) {
+        for (const cardIndex of draw) { // PERMET D'ENREGISTRER DANS L'INVENTAIRE (BDD) LES CARTES TIRER
             await prisma.inventaire.create({
                 data: {
                     ownerId: userId,
@@ -40,13 +40,13 @@ async function drawCards(req, res) {
             });
         }
 
-        const userInventaire = await prisma.inventaire.findMany({
+        const userInventaire = await prisma.inventaire.findMany({ // Récupère et retourne l'inventaire mis à jour de l'utilisateur
             where: { ownerId: userId },
             include: { card: true },
             orderBy: { cardId: 'asc' }
         });
 
-        res.json({ draw, userInventaire });
+        res.json({ draw, userInventaire }); // Renvoie le tirage et l'inventaire de l'utilisateur au client
     } catch (error) {
         console.log('Erreur lors du tirage:', error);
         res.status(500).send("Erreur serveur");
